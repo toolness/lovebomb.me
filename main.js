aa0cb6681a96cefd921ad0a3d77911c6d77523cc
Atul Varma <varmaa@gmail.com>
2011-12-09 14:12:19 -0800

    very buggy sliding on hash change

--- main.js
+++ main.js
@@ -4,6 +4,37 @@ function absolutifyURL(relativeURL) {
   return a[0].href;
 }
 
+function currentSection() {
+  return $("section").not(".out-on-right, .out-on-left")
+}
+
+function goTo(sectionID) {
+  var next = $("section" + sectionID);
+  var current = currentSection();
+  if (!next.length || next[0] == current[0])
+    return;
+  if (next.hasClass("out-on-right")) {
+    current.addClass("out-on-left");
+    next.show().removeClass("out-on-right");
+    next.prevAll(".out-on-right")
+      .hide().removeClass("out-on-right").addClass("out-on-left");
+  } else {
+    current.addClass("out-on-right");
+    next.show().removeClass("out-on-left");
+    next.prevAll(".out-on-left")
+      .hide().removeClass("out-on-left").addClass("out-on-right");
+  }
+}
+
+function goToHash() {
+  var hash = window.location.hash;
+  if (hash.length < 2)
+    hash = "#intro";
+  goTo(hash);
+}
+
+onhashchange = goToHash;
+
 var delay;
 var editor;
 var DELAY_MS = 300;
@@ -54,5 +85,6 @@ $(window).ready(function() {
       });
       updatePreview();
     });
-  }).click();
+  });
+  goToHash();
 });
