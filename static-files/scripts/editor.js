d159dc4d276d2a5fca3580e36ebdcebefeb28d02
Atul Varma <varmaa@gmail.com>
2012-01-20 10:26:29 +0900

    Initial support for colorpicker. Lots of bugs, but the general case works okay.

--- static-files/scripts/editor.js
+++ static-files/scripts/editor.js
@@ -57,6 +57,20 @@ var Editor = (function() {
     return html;
   }
 
+  var picker = null;
+  var pickerPos = null;
+  var inCursorActivity = false;
+  
+  function removePicker() {
+    if (picker) {
+      pickerPos = null;
+      picker.colorpicker("close");
+      picker.colorpicker("destroy");
+      picker.remove();
+      picker = null;
+    }
+  }
+  
   function getEditor() {
     if (typeof(editor) == "undefined")
       editor = CodeMirror(function(element) {
@@ -67,6 +81,95 @@ var Editor = (function() {
         tabMode: "indent",
         lineWrapping: true,
         lineNumbers: true,
+        onCursorActivity: function() {
+          if (inCursorActivity)
+            return;
+          var pos = editor.getCursor();
+          var token = editor.getTokenAt(pos);
+
+          function nextToken() {
+            var oldToken = token;
+            token = editor.getTokenAt({
+              line: pos.line,
+              ch: token.end + 1
+            });
+            return token.start != oldToken.start;
+          }
+
+          function prevToken() {
+            var oldToken = token;
+            token = editor.getTokenAt({
+              line: pos.line,
+              ch: token.start
+            });
+            return token.start != oldToken.start;
+          }
+
+          Editor.currToken = token;
+          Editor.currLine = editor.getLine(pos.line);
+          if (token.state && token.state.mode == "css" &&
+              token.state.localState.stack[0] == "{" &&
+              token.state.localState.stack[1] == "rule" &&
+              token.className != "variable" &&
+              token.string != ";" &&
+              token.string != ":") {
+            while (token.start != 0 && token.string != ":") {
+              prevToken();
+            }
+            nextToken();
+            var tokens = [token];
+            var tokensStart = token.start;
+            var tokensEnd = token.end;
+            while (nextToken() && token.string != ";") {
+              tokens.push(token);
+              tokensEnd = token.end;
+            }
+            var cssValue = tokens.map(function(t) {
+              return t.string
+            }).join("").trim();
+            if (pickerPos && pickerPos.line == pos.line &&
+                pickerPos.ch == tokensStart)
+              return;
+            removePicker();
+            pickerPos = {line: pos.line, ch: tokensStart};
+            //console.log("cssValue is", cssValue);
+            picker = $('<div class="picker"></div>').appendTo(document.body);
+            picker.colorpicker({
+              alpha: true,
+              color: cssValue,
+              onSelect: function(hex, rgba, inst) {
+                if (!pickerPos)
+                  return;
+                var value;
+                if (rgba.a == 1) {
+                  value = hex;
+                } else {
+                  value = 'rgba(' + Math.floor(rgba.r * 255) + ', ' +
+                                    Math.floor(rgba.g * 255) + ', ' +
+                                    Math.floor(rgba.b * 255) + ', ' +
+                                    rgba.a + ')';
+                }
+                inCursorActivity = true;
+                editor.replaceRange(value, {
+                  line: pos.line,
+                  ch: tokensStart + 1
+                }, {
+                  line: pos.line,
+                  ch: tokensEnd
+                });
+                tokensEnd = tokensStart + 1 + value.length;
+                inCursorActivity = false;
+              }
+            });
+            editor.addWidget({
+              line: pos.line,
+              ch: 0
+            }, picker[0], false);
+          } else {
+            removePicker();
+          }
+          return;
+        },
         onChange: function schedulePreviewRefresh() {
           if (nextUpdateIsSilent) {
             nextUpdateIsSilent = false;
