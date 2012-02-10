82075683d20d3c9dbc5191ed702693ac430b0b7b
Atul Varma <varmaa@gmail.com>
2012-02-10 11:51:47 -0500

    we now convert raw readability HTML to plaintext on the client-side. mini test suite included.

--- static-files/scripts/chooser.js
+++ static-files/scripts/chooser.js
@@ -1,9 +1,50 @@
+function convertToPlainText(html) {
+  var IGNORED_ELEMENTS = ["rss"];
+  var div = $('<div></div>');
+  var lines = [];
+  var currentLine = [];
+  div.html(html).appendTo(document.body);
+
+  function pushLine() {
+    if (currentLine.length) {
+      lines.push(currentLine.join(" "));
+      currentLine = [];
+    }
+  }
+  
+  function descend(element) {
+    var style = window.getComputedStyle(element);
+    var display = style.getPropertyValue("display");
+    if (display == "block" || display == "list-item")
+      pushLine();
+    for (var i = 0; i < element.childNodes.length; i++) {
+      var child = element.childNodes[i];
+      if (child.nodeType == child.ELEMENT_NODE &&
+          $.inArray(child.nodeName.toLowerCase(), IGNORED_ELEMENTS) == -1) {
+        descend(child);
+      } else if (child.nodeType == child.TEXT_NODE) {
+        var text = jQuery.trim(child.nodeValue);
+        if (text.length)
+          currentLine.push(text);
+      }
+    }
+  }
+  
+  descend(div[0]);
+  pushLine();
+  div.remove();
+  return lines.join('\n\n');
+}
+
 (function(jQuery) {
   var BASE_URL = "http://bjb.io:9123/";
   //var BASE_URL = "http://localhost:8080/";
   $(window).ready(function() {
     var val = "";
     var req = null;
+    if (!$("section#chooser input").length)
+      // We're in the test suite, apparently.
+      return;
     setInterval(function() {
       var newVal = $("section#chooser input").val();
       if (newVal.trim().length == 0) {
@@ -20,9 +61,10 @@
           url: BASE_URL + 'article',
           data: {
             url: val,
+            raw: true
           },
-          success: function(data) {
-            data = data.trim();
+          success: function(html) {
+            var data = convertToPlainText(html);
             if (data.length == 0) {
               $("section#chooser .error.result").fadeIn();
             } else {
